7af0cacc319bfb9e76a1bd57f5f3a26d80caccfc
Ciaron Howell <ciaron.howell@stfc.ac.uk>
2023-07-12 14:57:30 +0100

    Fixed missing images + updated gitignore file for website

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,18 +2,22 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import markdoc from "@astrojs/markdoc";
 import react from "@astrojs/react";
 
 import node from "@astrojs/node";
 
+// Seems better to use the experimental image feature - https://github.com/withastro/astro/issues/5008
+//
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    assets: true,
+  },
   output: "hybrid",
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), image(), markdoc(), react()],
+  integrations: [mdx(), sitemap(), tailwind(), markdoc(), react()],
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
